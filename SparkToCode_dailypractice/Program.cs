c95cca6cefe537868d21a0d7c0343b5b79e2c9ac
Aseel <aswahaibi8@gmail.com>
2026-07-05 15:28:18 +0400

    practiced on Math.Pow function

--- SparkToCode_dailypractice/Program.cs
+++ SparkToCode_dailypractice/Program.cs
@@ -8,9 +8,23 @@ class Program
         //built-in functions - 1- math functions 
         // math.abs --> making the (-) value (+)
         
+        /*
         Console.Write("enter a negative number:  ");
         decimal userInput = Math.Abs(decimal.Parse(Console.ReadLine()));
         decimal result = userInput;
         Console.WriteLine("absolute result is :  "+result);
+        */
+        
+        // Math.Pow --> getting the power of a number (2^3)
+        
+        Console.Write("enter the base:  ");
+        float baseValue = float.Parse(Console.ReadLine());
+        Console.Write("enter the power:  ");
+        float powervalue = float.Parse(Console.ReadLine());
+        
+        double result = Math.Pow(baseValue, powervalue);
+        Console.WriteLine("this is the result of power:  "+result);
+        
+        
     }
 }
