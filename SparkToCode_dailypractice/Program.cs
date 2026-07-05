4b9b0f5247f9b4078a55e90be9b8ba11768746b5
Aseel <aswahaibi8@gmail.com>
2026-07-05 15:20:18 +0400

    practiced on Math.Abs function

--- SparkToCode_dailypractice/Program.cs
+++ SparkToCode_dailypractice/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // 5/7/25 / 3rd session - functions 
+        //built-in functions - 1- math functions 
+        // math.abs --> making the (-) value (+)
+        
+        Console.Write("enter a negative number:  ");
+        decimal userInput = Math.Abs(decimal.Parse(Console.ReadLine()));
+        decimal result = userInput;
+        Console.WriteLine("absolute result is :  "+result);
     }
 }
